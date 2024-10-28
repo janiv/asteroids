aef92949860213034bfd0147e6a5ea1300671aa7
janiv <jani.vilkki@gmail.com>
2024-10-28 00:32:49 -0500

    Test on mint

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 import pygame
+from constants import *
 
 def main():
-    print('Starting asteroids!')
+    pygame.init()
+    BLACK = (0, 0, 0)
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen.fill(BLACK)
+    running = True
+    while(running):
+        screen.fill(BLACK)
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+    pygame.quit()
 
 if __name__ == "__main__":
     main()
