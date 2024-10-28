5abf4984fedf42411f148676b6e9e55cf2535642
janiv <jani.vilkki@gmail.com>
2024-10-28 12:45:24 -0500

    Now drawing triangle

--- main.py
+++ main.py
@@ -1,18 +1,31 @@
 import pygame
 from constants import *
+from player import *
 
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
+    dt = 0
+
+
     BLACK = (0, 0, 0)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    screen.fill(BLACK)
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    player = Player(x, y)
+
+
     running = True
     while(running):
         screen.fill(BLACK)
+        player.draw(screen)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+        delta_time = clock.tick(60)
+        dt = delta_time / 1000
     pygame.quit()
 
 if __name__ == "__main__":
